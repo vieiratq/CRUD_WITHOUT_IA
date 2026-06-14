e4a054c2439f6df0c5dbbc14c205cfaa51290046
Joao pedro vieira <joaovieirapedro1@gmail.com>
2026-06-14 12:59:38 -0300

    postgree implementado e pastas de rotas organizadas

---
+++ routes/authRoutes.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const path = require("path");
+const db = require("./databaseroutes");
+const router = express.Router();
+function ValidaLogin(req, res, next) {
+  if (req.session.user)
+    return next()
+  else
+    res.redirect("/")
+}
+function checktamanho(item, min, max) {
+  if (item.length < min || item.length > max)
+    return false
+  else
+    return true
+}
+
+
+router.post("/register", (req, res) => {
+  const { username, email, password } = req.body;
+  if (!checktamanho(username, 5, 20))
+    return res.json({ success: false, message: "username deve conter entre 3 e 20 caracteres" })
+  if (!checktamanho(password, 6, 18))
+    return res.json({ success: false, message: "password deve conter entre 6 e 50 caracteres" })
+  if (!checktamanho(email, 5, 25))
+    return res.json({ success: false, message: "email deve conter entre 5 e 50 caracteres" })
+
+  db.query("SELECT username, email FROM users WHERE username = $1 OR email = $2", [username, email], (err, result) => {
+    if (err)
+      return res.json({ success: false, message: "erro ao buscar dados" })
+    const user = result.rows[0]
+    if (user && user.email === email)
+      return res.json({ success: false, message: "Email ja cadastrato, use outro email" })
+    if (user && user.username === username)
+      return res.json({ success: false, message: "username ja cadastrato, use outro username" })
+    db.query("INSERT INTO users(username,email,password) VALUES($1,$2,$3)", [req.body.username, req.body.email, req.body.password], (err) => {
+      if (err)
+        return res.json({ success: false, message: "erro ao cadastrar" })
+      return res.json({ success: true, message: "usuario cadastrado" }), console.log("usuario cadastrado com sucesso" + username + " " + email + ' ' + password)
+    })
+
+  })
+
+
+})
+
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  db.query("SELECT id, username, email FROM users WHERE email = $1 AND password = $2", [email, password],
+    (err, result) => {
+
+      if (err)
+        return res.json({ success: false, message: "Erro no login" })
+      const user = result.rows[0]
+      if (!user)
+        return res.json({ success: false, message: "Email ou senha incorreto" })
+      if (user)
+        req.session.user = { id: user.id, email: user.email, logado: true }, console.log("usuario Logado com sucesso " + email + " " + password)
+      return res.json({
+        success: true,
+        message: "usuario ENCONTRADO",
+        error: "1",
+        id: user.id,
+        email: user.email,
+        name: user.username
+      })
+    })
+
+})
+
+router.get("/logout", (req, res) => {
+  req.session.destroy()
+  res.redirect("/")
+})
+
+router.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "register.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 const session = require("express-session");
-const { Pool } = require("pg");
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
+const authRoutes = require("./routes/authRoutes");
 ////////////////////////////////////////////////////////////////////////
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "private")));
@@ -20,10 +14,6 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
-app.get("/logout", (req, res) => {
-  req.session.destroy()
-  res.redirect("/")
-})
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
@@ -31,81 +21,20 @@ app.get("/", (req, res) => {
 
 app.get("/dashboard", ValidaLogin, (req, res) => {
   res.sendFile(path.join(__dirname, "backend", "dashboard", "dash.html"))
-})
-
-
-app.use(express.json());
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "register.html"));
 });
 
 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  db.query("SELECT id, username, email FROM users WHERE email = $1 AND password = $2", [email, password],
-    (err, result) => {
-
-      if (err)
-        return res.json({ success: false, message: "Erro no login" })
-      const user = result.rows[0]
-      if (!user)
-        return res.json({ success: false, message: "Email ou senha incorreto" })
-      if (user)
-        req.session.user = { id: user.id, email: user.email, logado: true }, console.log("usuario Logado com sucesso " + email + " " + password)
-      return res.json({
-        success: true,
-        message: "usuario ENCONTRADO",
-        error: "1",
-        id: user.id,
-        email: user.email,
-        name: user.username
-      })
-    })
-
-})
-
-
-app.post("/register", (req, res) => {
-  const { username, email, password } = req.body;
-  if (!checktamanho(username, 3, 20))
-    return res.json({ success: false, message: "username deve conter entre 3 e 20 caracteres" })
-  if (!checktamanho(password, 6, 50))
-    return res.json({ success: false, message: "password deve conter entre 6 e 50 caracteres" })
-  if (!checktamanho(email, 5, 50))
-    return res.json({ success: false, message: "email deve conter entre 5 e 50 caracteres" })
-
-  db.query("SELECT username, email FROM users WHERE username = $1 OR email = $2", [username, email], (err, result) => {
-    if (err)
-      return res.json({ success: false, message: "erro ao buscar dados" })
-    const user = result.rows[0]
-    if (user && user.email === email)
-      return res.json({ success: false, message: "Email ja cadastrato, use outro email" })
-    if (user && user.username === username)
-      return res.json({ success: false, message: "username ja cadastrato, use outro username" })
-    db.query("INSERT INTO users(username,email,password) VALUES($1,$2,$3)", [req.body.username, req.body.email, req.body.password], (err) => {
-      if (err)
-        return res.json({ success: false, message: "erro ao cadastrar" })
-      return res.json({ success: true, message: "usuario cadastrado" }), console.log("usuario cadastrado com sucesso" + username + " " + email + ' ' + password)
-    })
-
-  })
-
-
-})
-
+app.use(express.json());
+app.use("/", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+
 function ValidaLogin(req, res, next) {
   if (req.session.user)
     return next()
   else
     res.redirect("/")
-}
-function checktamanho(item, min, max) {
-  if (item.length < min || item.length > max)
-    return false
-  else
-    return true
 }
